49588bb2630bbaaa88bbdb412ed83151034f888b
Brandon <bbabu001@ucr.edu>
2020-05-01 12:57:37 -0700

    lab 7 done

---
+++ Lab7/source/main.c
@@ -0,0 +1,202 @@
+/*	Author: brandon babu
+ *  Partner(s) Name: 
+ *	Lab Section: 27
+ *	Assignment: Lab #7  Exercise #2
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.h"
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+enum States {Start, s1, s2, s3, hold1, hold2} state;
+unsigned char b;
+unsigned char a;
+unsigned char dir;
+unsigned char press;
+unsigned char score;
+
+volatile unsigned char TimerFlag = 0; //TimerISR() sets this to 1. C programmer should clear to 0
+
+//internal variables for mapping AVR's ISR to our cleaner TimerISR() model.
+unsigned long _avr_timer_M = 1; //start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; //Current internal count of 1ms ticks
+
+void TimerOn(){
+	//AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B; 
+
+	//AVR output compare register OCR1A
+	OCR1A = 125; 
+
+	//AVR timer interrupt mask register
+	TIMSK1 = 0x02; //bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	//	TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; //0x80: 1000000
+
+}
+
+void TimerOff(){
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR(){
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+
+}
+
+//Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M){
+
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+
+}
+
+void TickSM() {
+	switch(state) {
+		case Start:
+			state = s1;
+			break;
+		case s1:
+			if (a == 0x01 && press == 0) {
+				state = hold1;
+			}
+			else {
+				if (a == 0x00) {
+					press = 0;
+				}
+				state = s2;
+			}
+			break;
+		case s2:
+			if (a == 0x01 && press == 0) {
+				state = hold1;
+				if (score < 9) {
+					score++;
+				}
+			}
+			else {
+				if (dir == 1) {
+					if (a == 0x00) {
+						press = 0;
+					}
+					state = s3;
+				}
+				else {
+					if (a == 0x00) {
+						press = 0;
+					}
+					state = s1;
+				}
+				if (score > 0) {
+					score--;
+				}
+			}
+			break;
+		case s3:
+			if (a == 0x01 && press == 0) {
+				state = hold1;
+			}
+			else {
+				if (a == 0x00) {
+					press = 0;
+				}
+				state = s2;
+			}
+			break;
+		case hold1:
+			if (a == 0x00) {
+				state = hold2;
+			}
+			break;
+		case hold2:
+			if (a == 0x01) {
+				state = s1;
+				press = 1;
+			}
+			break;
+		default:
+			state = s1;
+			break;
+	}
+	switch(state) {
+		case Start:
+			b = 0x01;
+			dir = 0;
+			score = 5;
+			break;
+		case s1:
+			b = 0x01;
+			dir = 1;
+			break;
+		case s2:
+			b = 0x02;
+			break;
+		case s3:
+			b = 0x04;
+			dir = 0;
+			break;
+		case hold1:
+			break;
+		case hold2:
+			break;
+		default:
+			b = 0x01;
+			score = 5;
+			break;
+	}
+}
+
+int main() {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	TimerSet(300);
+	TimerOn();
+	b = 0x00;
+	a = 0x00;
+	score = 5;
+	state = Start;
+	LCD_init();
+
+	while(1){
+		a = ~PINA & 0x01;
+		TickSM();
+		while(!TimerFlag); //Wait 1 sec
+		TimerFlag = 0;
+		PORTB = b;
+		if (score == 9) {
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "You Win!");
+		}
+		else {
+			LCD_ClearScreen();
+			LCD_Cursor(1);
+			LCD_WriteData(score + '0');
+		}
+	}
+	return 0;
+}
---
+++ Lab7/turnin/bbabu001_lab7_part1.c
@@ -0,0 +1,183 @@
+/*	Author: bbabu001
+ *  Partner(s) Name: 
+ *	Lab Section: 027
+ *	Assignment: Lab #7  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "io.h"
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, init, pressA0, pressA1, wait, bothPress} state;
+unsigned char a, b, t, s;
+
+volatile unsigned char TimerFlag = 0; //TimerISR() sets this to 1. C programmer should clear to 0
+
+//internal variables for mapping AVR's ISR to our cleaner TimerISR() model.
+unsigned long _avr_timer_M = 1; //start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; //Current internal count of 1ms ticks
+
+void TimerOn(){
+	//AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B; 
+
+	//AVR output compare register OCR1A
+	OCR1A = 125; 
+
+	//AVR timer interrupt mask register
+	TIMSK1 = 0x02; //bit1: OCIE1A -- enables compare match interrupt
+
+	//Initialize avr counter
+	TCNT1 = 0;
+
+	_avr_timer_cntcurr = _avr_timer_M;
+	//	TimerISR will be called every _avr_timer_cntcurr milliseconds
+
+	//Enable global interrupts
+	SREG |= 0x80; //0x80: 1000000
+
+}
+
+void TimerOff(){
+	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+}
+
+void TimerISR(){
+	TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect){
+
+	_avr_timer_cntcurr--;
+	if(_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+
+}
+
+//Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M){
+
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+
+}
+
+void Tick() {
+
+	switch(state) { // Transitions
+		case Start:
+			state = init;
+		case init:
+			if (a == 0x01) {
+				state = pressA0;
+				if (b < 9) { b++; }
+			}
+			else if (a == 0x02) {
+				state = pressA1;
+				if (b > 0) { b--; }
+			}
+			else if (a == 0x03) {state = bothPress; }
+			else { state = init; }
+			break;
+		case pressA0:
+			if (a == 0x00) { state = wait; }
+			else if (a == 0x03) { state = bothPress; }
+			else { state = pressA0; }
+			break;
+		case pressA1:
+			if (a == 0x00) { state = wait; }
+			else if (a == 0x03) { state = bothPress; }
+			else { state = pressA1; }
+			break;
+		case wait:
+			if (a == 0x01) {
+                                state = pressA0;
+				if (b < 9) { b++; }
+                        }
+			else if (a == 0x02) {
+                                state = pressA1;
+				if (b > 0) { b--; }
+			}
+			else if (a == 0x03) {state = bothPress; }
+                        else { state = wait; }
+			break;
+		case bothPress:
+			if (a == 0x03) { state = bothPress; }
+			else { state = wait; }
+			break;
+		default:
+			state = Start;
+			break;
+	}
+	
+	switch(state) {	// State Actions
+		case Start:
+			b = 0x00;
+			break;
+		case init:
+			b = 0x00;
+			t = 0;
+			s = 1;
+			break;
+		case pressA0:
+			if (b < 9 && t == 10) {
+				b++;
+				t = 0;
+			}
+			t++;
+			s = 2;
+			break;
+		case pressA1:
+			if (b >= 1 && t == 10) {
+				b--;
+				t = 0;
+			}
+			t++;
+			s = 3;
+			break;
+		case wait:
+			t = 0;
+			s = 4;
+			break;
+		case bothPress:
+			b = 0x00;
+			t = 0;
+			s = 5;
+			break;		
+		default:
+			b = 0x00;
+			t = 0;
+			break;	
+	}
+}
+
+int main(void) {
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+
+	LCD_init();
+	TimerSet(100);
+	TimerOn();
+	b = 0x07;
+	state = init;
+    while (1) {
+	a = ~PINA & 0x03;
+	Tick();
+	while(!TimerFlag); //Wait 1 sec
+	TimerFlag = 0;
+	PORTB = s;
+	LCD_Cursor(1);
+	LCD_WriteData(b + '0');
+    }
+    return 0;
+}
